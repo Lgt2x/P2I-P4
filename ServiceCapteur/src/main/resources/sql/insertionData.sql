b9dda64b5cd4f77f9732444b6007665589d40614
Louis Gombert <louisgombert@gmail.com>
2020-06-04 12:18:04 +0200

    Fix data transmission between PT and Jaba

--- ServiceCapteur/src/main/resources/sql/insertionData.sql
+++ ServiceCapteur/src/main/resources/sql/insertionData.sql
@@ -11,7 +11,7 @@ VALUES ('Température', 'Degré Celsius', '°C'),
        ('Humidité', 'Pourcentage', '%'),
        ('Luminosité', 'Lux', 'lux'),
        ('Concentration CO2', 'Parties par million', 'ppm'),
-       ('Concentration NO2', 'Parties par million', 'ppm'),
+       ('Concentration O2', 'Parties par million', 'ppm'),
        ('Bruit', 'Décibel', 'dB'),
        ('Particules fines', 'Parties par million', 'ppm'),
        ('Pression', 'Pascal', 'Pa');
@@ -32,48 +32,22 @@ VALUES (45.625536, 5.132806, 'Maison Maxou'),
 
 INSERT INTO station (nomStation)
 VALUES ('Station Charlie'),
-        ('Station Barkhane'),
-        ('Station Phoenix'),
-        ('Station Tango');
+        ('Station Barkhane');
 
 INSERT INTO installation (dateDebut, dateFin, idLocalisation, idStation)
 VALUES ('2020-05-01', '2020-05-9', 1, 1),
        ('2020-05-10', '2020-06-01', 7, 1),
        ('2020-05-01', '2020-05-19', 2, 2),
-       ('2020-05-20', '2020-06-01', 5, 2),
-       ('2020-05-01', '2020-05-28', 2, 3),
-       ('2020-05-29', '2020-06-01', 2, 3),
-       ('2020-05-01', '2020-05-15', 7, 4),
-       ('2020-05-16', '2020-06-01', 7, 4);
+       ('2020-05-20', '2020-06-01', 5, 2);
 
 
 INSERT INTO capteur (idTypeCapteur, idStation)
     VALUES
-           (1, 1),
-           (2, 1),
-           (3, 1),
-           (4, 1),
-           (5, 1),
            (6, 1),
-           (7, 1),
-           (1, 2),
-           (2, 2),
-           (3, 2),
+           (3, 1),
+           (2, 1),
+           (1, 1),
            (4, 2),
-           (5, 2),
-           (6, 2),
            (7, 2),
-           (1, 3),
-           (2, 3),
-           (3, 3),
-           (4, 3),
-           (5, 3),
-           (6, 3),
-           (7, 3),
-           (1, 4),
-           (2, 4),
-           (3, 4),
-           (4, 4),
-           (5, 4),
-           (6, 4),
-           (7, 4);
+           (8, 2),
+           (5, 2);
