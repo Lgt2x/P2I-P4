c63b63c70a952ebcfc84e2a39229130a4d990c80
LimelioN <memorial.limelion@gmail.com>
2020-06-05 16:09:03 +0200

    Simplify BiQuantityDataSet

--- Interface/src/main/java/BiQuantityDataSet.java
+++ Interface/src/main/java/BiQuantityDataSet.java
@@ -1,7 +1,6 @@
 import org.knowm.xchart.QuickChart;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYSeries;
-import org.knowm.xchart.internal.chartpart.Chart;
 import org.knowm.xchart.style.XYStyler;
 import org.knowm.xchart.style.markers.SeriesMarkers;
 
@@ -9,7 +8,7 @@ import java.awt.Color;
 
 public class BiQuantityDataSet extends DataSet {
 
-    public BiQuantityDataSet(LectureBase bd, String stationName,  String typeX, String typeY) throws Exception{
+    public BiQuantityDataSet(LectureBase bd, String stationName, String typeX, String typeY) throws Exception {
 
         super(bd, stationName, typeX, typeY);
 
@@ -19,7 +18,7 @@ public class BiQuantityDataSet extends DataSet {
         Object[] xDataInfo = source.getDataTypeInfo(typeX);
         Object[] yDataInfo = source.getDataTypeInfo(typeY);
 
-        if(xDataInfo == null || yDataInfo == null)
+        if (xDataInfo == null || yDataInfo == null)
             throw new Exception("Erreur construction du dataset, la DB n'a pas renvoyé un résultat correct");
 
         units[0] = xDataInfo[0].toString();
@@ -33,26 +32,23 @@ public class BiQuantityDataSet extends DataSet {
     }
 
     @Override
-    public Chart makeChart() {
+    public XYChart makeChart() {
 
         if (dataset == null || dataset[0] == null || dataset[1] == null || dataset[0].length != dataset[1].length || dataset[0].length == 0)
             return null;
 
-        Chart chart = QuickChart.getChart("Graphique",
-                                    typeX + " (" + units[0] + ")",
-                                    typeY + " (" + units[1] + ")",
-                                    stationName,
-                                    dataset[0], dataset[1]);
+        XYChart chart = QuickChart.getChart("Graphique",
+                typeX + " (" + units[0] + ")",
+                typeY + " (" + units[1] + ")",
+                stationName,
+                dataset[0], dataset[1]);
 
-        XYStyler styler = ((XYChart) chart).getStyler();
+        XYStyler styler = chart.getStyler();
         styler.setDefaultSeriesRenderStyle(graphType);
-        chart.getSeriesMap().values().stream().forEach(series -> {
-
-            if (series instanceof XYSeries) {
-                XYSeries xySeries = (XYSeries) series;
-
-                xySeries.setMarker(SeriesMarkers.PLUS);
-                xySeries.setMarkerColor(Color.blue);
+        chart.getSeriesMap().values().forEach(series -> {
+            if (series != null) {
+                series.setMarker(SeriesMarkers.PLUS);
+                series.setMarkerColor(Color.blue);
             }
         });
 
